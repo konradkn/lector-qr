315b4d7aeb2e290f7ba6afcc4c8928ed0db6e289
Konrad Knochenhauer <103902628+konradkn@users.noreply.github.com>
2025-07-08 15:27:33 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [qrValue, setQrValue] = useState("")
+
+  return (
+    <>
+      <div>
+        <h1>Lector QR</h1>
+        <p>{qrValue}</p>
+        <input
+        autoFocus
+        onChange={(e) => setQrValue(e.value.target)}
+        value={qrValue}
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
